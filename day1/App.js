4d0455d450db6c300b7828a18bbf9ffc2b61b4c9
Raneen Mahmoud <raneenm260@gmail.com>
2023-06-14 21:50:07 +0200

    day2

--- day1/App.js
+++ day1/App.js
@@ -8,9 +8,14 @@ export default function App() {
       {/* <View style={{height:600 }}>
      <Image source={require('./assets/icon.png')}></Image>
       </View> */}
-      {/* <View style={{height:600 }}>
-     <Image style={{width:300,height:600}}  source={require('./assets/Images/pict 2.jpg')}></Image>
-      </View> */}
+      <View style={{width:320}}>
+     <Image style={{width:300,height:600}} source={{uri:"https://images.unsplash.com/photo-1685115910523-2516c451101b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=407&q=80",
+    }}></Image>
+      </View>
+
+      <View style={{height:700 }}>
+     <Image style={{width:300,height:600}} source={require('./assets/Images/2.jpg')}></Image>
+      </View>
 
     <View style={{width:320}}>
      <Image style={{width:300,height:600}} source={{uri:"https://images.unsplash.com/photo-1685115910523-2516c451101b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=407&q=80",
